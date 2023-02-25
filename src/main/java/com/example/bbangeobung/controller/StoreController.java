5a28366ef770dcec281f21e8acbf31ec2ce1793f
kikidy12 <kikidy12@naver.com>
2023-02-25 17:14:45 +0900

    FIX: 상점 api 수정 swagger store관련해서 명세 추가 상점 삭제 추가 상점 삭제시 관리자나 등록한 사람만 가능하도록 수정

--- src/main/java/com/example/bbangeobung/controller/StoreController.java
+++ src/main/java/com/example/bbangeobung/controller/StoreController.java
@@ -3,9 +3,12 @@ package com.example.bbangeobung.controller;
 import com.example.bbangeobung.common.dto.ResponseDto;
 import com.example.bbangeobung.dto.StoreDto;
 import com.example.bbangeobung.entity.Store;
+import com.example.bbangeobung.repository.StoreRepository;
 import com.example.bbangeobung.security.UserDetailsImpl;
 import com.example.bbangeobung.service.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -25,22 +28,37 @@ public class StoreController {
     private final StoreService storeService;
 
     @GetMapping("/")
+    @Operation(summary = "상점 목록 조회", description = "상점 목록 조회")
     @SecurityRequirements()
     public ResponseDto<List<StoreDto.StoreRes>> getStores(
     ) {
         return ResponseDto.of(HttpStatus.OK, "조회 성공", storeService.getStores());
     }
     @GetMapping("/{storeId}")
+    @Operation(summary = "상점 조회", description = "상점 조회")
     @SecurityRequirements()
     public ResponseDto<StoreDto.StoreRes> getStore(@PathVariable Long storeId) {
         return ResponseDto.of(HttpStatus.OK, "조회 성공", storeService.getStore(storeId));
     }
 
     @PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "상점 등록", description = "상점 등록 token필요")
     public ResponseDto<StoreDto.StoreRes> addStore(
             @ModelAttribute StoreDto.StoreAdd dto,
             @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
         return ResponseDto.of(HttpStatus.OK, "등록 성공", storeService.addStore(dto, userDetails.getUser()));
+    }
+
+    @DeleteMapping(value = "/{storeId}")
+    @Operation(summary = "상점 삭제", description = "상점 삭제 관리자나 등록한 사람만 삭제 가능")
+    public ResponseDto deleteStore(
+            @PathVariable Long storeId,
+            @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails
+            ) {
+
+        storeService.deleteStore(storeId, userDetails.getUser());
+
+        return ResponseDto.of(HttpStatus.OK, "삭제성공");
     }
 }
