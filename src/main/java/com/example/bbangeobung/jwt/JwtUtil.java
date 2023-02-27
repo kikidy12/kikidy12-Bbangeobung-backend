c9ee9bb465ea7d90c4c9d4c81d55158e1abba32d
kikidy12 <kikidy12@naver.com>
2023-02-27 14:56:22 +0900

    FIX: token시간 연장

--- src/main/java/com/example/bbangeobung/jwt/JwtUtil.java
+++ src/main/java/com/example/bbangeobung/jwt/JwtUtil.java
@@ -30,7 +30,7 @@ public class JwtUtil {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String AUTHORIZATION_KEY = "auth";
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final long TOKEN_TIME = 60 * 60 * 1000L;
+    private static final long TOKEN_TIME = 24 * 60 * 60 * 1000L;
 
     private final UserDetailsServiceImpl userDetailsService;
 
