661533d7cd8580d5608fd7d77d3a2ec200a54bed
kikidy12 <kikidy12@naver.com>
2023-02-28 14:37:28 +0900

    FIX: 상점 좋아요 등록 swagger명세 추가 상점 좋아요 등록 swagger명세 추가

--- src/main/java/com/example/bbangeobung/controller/StoreLikeController.java
+++ src/main/java/com/example/bbangeobung/controller/StoreLikeController.java
@@ -3,12 +3,15 @@ package com.example.bbangeobung.controller;
 import com.example.bbangeobung.common.dto.ResponseDto;
 import com.example.bbangeobung.security.UserDetailsImpl;
 import com.example.bbangeobung.service.StoreLikeService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "StoreLike")
 @RestController
 @RequestMapping("/api/store")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class StoreLikeController {
     private final StoreLikeService storeLikeService;
 
     @PostMapping ("/like/{storeId}")
+    @Operation(summary = "상점 좋아요 등록/취소", description = "상점 좋아요 등록/취소 좋아요 등록은 true, 취소는 false")
     public ResponseDto<Boolean> likeStore(@PathVariable Long storeId, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Boolean isLike = storeLikeService.likeStore(storeId, userDetails.getUser());
 
